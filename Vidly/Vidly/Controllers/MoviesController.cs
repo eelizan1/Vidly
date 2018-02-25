422c2c0dff3924eba3c5c41c1837b3213853e686
Elizan <EJE0101@daugherty.com>
2018-02-24 21:03:04 -0500

    added files

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models; 
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            // create instance of movie model
+            // import Vidly.Models to use 
+            Movie movie = new Movie() { Name = "Shrek!" }; // the Name property is spelled as the model's property, Name
+            // render view with the model, movie
+            // return View(movie);
+            return Content("Hello, World");
+        }
+    }
+}
---
+++ Vidly/Vidly/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        // movie properties 
+        public int Id
+        {
+            get;
+            set; 
+        }
+
+        public String Name
+        {
+            get;
+            set; 
+        }
+    }
+}
